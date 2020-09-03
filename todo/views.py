9adbaba05fba4a1f83422fca746d76a607d9f60c
asifdanishyar <asifdanishyar@yahoo.com>
2020-09-03 19:10:50 +0000

    name item changed in add item route and working now

--- todo/views.py
+++ todo/views.py
@@ -7,14 +7,14 @@ from .models import Item
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
-        'item': items
+        'items': items
     }
     return render(request, 'todo/todo_list.html', context)
 
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
+        name = request.POST.get('name_item')
         done = 'done' in request.POST
         Item.objects.create(name=name, done=done)
 
