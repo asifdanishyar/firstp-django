5af3f25ee015bc4424c91c6119ce2bf8ce97b29a
asifdanishyar <asifdanishyar@yahoo.com>
2020-09-07 20:23:29 +0000

    tests are debugged and working now

--- todo/test_views.py
+++ todo/test_views.py
@@ -25,15 +25,15 @@ class TestViews(TestCase):
         self.assertRedirects(respons, '/')
 
     def test_can_delete_item(self):
-        item = Item.objects.create(name='Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item', done=False)
         respons = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(respons, '/')
-        existing_items = item.objects.filter(id=item.id)
+        existing_items = Item.objects.filter(id=item.id)
         self.assertEqual(len(existing_items), 0)
 
     def test_toggle_item(self):
         item = Item.objects.create(name='Test Todo Item', done=True)
         respons = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(respons, '/')
-        updated_item = item.objects.get(id=item.id)
+        updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
